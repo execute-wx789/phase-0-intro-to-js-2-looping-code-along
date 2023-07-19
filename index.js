85ea28bd847a6384a56645f54f13763eb16cc0c5
Yurick <YJakse789@gmail.com>
2023-07-19 09:51:55 -0400

    Completed Lab

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(names){
+    let returnArray = []
+    for(let i = 0; i < names.length; i++){
+        returnArray.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`)
+    }
+    return returnArray
+}
+function countDown(number){
+    let t = number
+    while(t !== -1){
+        console.log(t)
+        t--
+    }
+}
